83464f27c0e05864aeac9070c2190f0ca61b8acb
Satyam Jindal <satyamjindal072@gmail.com>
2018-05-16 00:08:24 +0530

    Create Game.cpp

---
+++ codeforces/Game.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++) cin>>a[i];
+	sort(a,a+n);
+	if(n%2!=0) cout<<a[n/2];
+	else cout<<a[(n/2)-1];
+}
