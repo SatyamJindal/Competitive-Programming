1279902753a5dc412a2824dbf5939234fb91f5ef
Satyam Jindal <satyamjindal072@gmail.com>
2018-06-04 15:02:02 +0530

    Create Fancy fence.cpp

---
+++ codeforces/Fancy fence.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int a;
+		cin>>a;
+		if(360%(180-a)==0) cout<<"YES"<<"\n";
+		else cout<<"NO"<<"\n";
+	}
+}
