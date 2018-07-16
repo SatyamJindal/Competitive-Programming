8ed52d7beaf8b7d8350304995d1ac1e018b70436
Satyam Jindal <satyamjindal072@gmail.com>
2018-07-17 00:03:06 +0530

    Create Sort an array of 0s, 1s and 2s.cpp

---
+++ Geeks/Sort an array of 0s, 1s and 2s.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++) cin>>a[i];
+		sort(a,a+n);
+		for(int i=0;i<n;i++) cout<<a[i]<<" ";
+		cout<<"\n";
+	}
+}
