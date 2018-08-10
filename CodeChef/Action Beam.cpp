59b82a9b2659cc06389c3893899e20b789794fee
Satyam Jindal <satyamjindal072@gmail.com>
2018-08-11 01:43:04 +0530

    Create Action Beam.cpp

---
+++ CodeChef/Action Beam.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+		int s,h;
+		cin>>s>>h;
+		if(s>h) cout<<"Shinchan"<<"\n";
+		else cout<<"Himawari"<<"\n";
+	}
+}
