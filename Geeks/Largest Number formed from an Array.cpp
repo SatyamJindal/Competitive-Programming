6844f54c1691c4d3bd0d42a2bc823366ff9f0d99
Satyam Jindal <satyamjindal072@gmail.com>
2019-01-30 02:29:13 +0530

    Update Largest Number formed from an Array.cpp

--- Geeks/Largest Number formed from an Array.cpp
+++ Geeks/Largest Number formed from an Array.cpp
@@ -3,41 +3,33 @@ using namespace std;
 
 bool cmp(int a,int b)
 {
-	int num,num1,t=1,t1=1;
-	while(t<=b)
-		t*=10;
-	while(t1<=a)
-		t1*=10;
-	return ((a*t+b)>=(a+b*t1));
+    int num1,num2;
+    int l1=1,l2=1;
+    while(l1<=b)
+        l1*=10;
+    while(l2<=a)
+        l2*=10;
+    num1 = a*l1 + b;
+    num2 = b*l2 + a;
+    return (num1>num2);
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	int t;
-	cin>>t;
-	
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		int a[n];
-		for(int i=0;i<n;i++)
-			cin>>a[i];
-		sort(a,a+n,cmp);
-		string ans;
-		for(int i=0;i<n;i++)
-		{
-			int val=a[i];
-			string curr;
-			while(val)
-			{
-				curr+=((val%10) + '0');
-				val/=10;
-			}
-			reverse(curr.begin(),curr.end());
-			ans+=curr;
-		}
-		cout<<ans<<"\n";
-	}
+    ios_base::sync_with_stdio(false);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        sort(a,a+n,cmp);
+        string ans;
+        for(int i=0;i<n;i++)
+            ans+=to_string(a[i]);
+        cout<<ans<<"\n";
+    }
 }
